4c2fc42e82cfd96cf6d9f239a7c1e67679e3a440
Sophia <83651475+sophialai123@users.noreply.github.com>
2023-02-18 16:56:18 +1100

    Create FindSmallestInt.cs

---
+++ C#/Codewars/8kyu/FindSmallestInt.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Linq;
+using System.Text;
+
+/*
+Given an array of integers your solution should find the smallest integer.
+Given [34, 15, 88, 2] your solution will return 2
+ */
+
+public static class Kata
+{
+    static void Main(string[] args)
+    {
+       int [] number = {34, -345, -1, 100};
+
+       var result = FindSmallestInt(number);
+
+        Console.WriteLine(result);
+
+    }
+
+    public static int FindSmallestInt(int[] args) => args.OrderBy(x => x).FirstOrDefault();
+   
+
+}
