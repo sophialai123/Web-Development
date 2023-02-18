a7f8afa15fe01177ee7e79ef9e3954b8eec7b52d
Sophia <83651475+sophialai123@users.noreply.github.com>
2023-02-18 16:33:17 +1100

    Create ASCIITotal.cs

---
+++ C#/Codewars/8kyu/ASCIITotal.cs
@@ -0,0 +1,46 @@
+/*
+ You'll be given a string, and have to return the sum of all characters as an int. The function should be able to handle all printable ASCII characters.
+  uniTotal("a") == 97
+  uniTotal("aaa") == 291
+https://www.ascii-code.com/
+ */
+
+public static class Kata
+{
+    static void Main(string[] args)
+    {
+
+        var result = UniTotal("aaa");
+        Console.WriteLine(result);
+
+        var str = "aa";
+
+        Console.WriteLine(TotalSun(str)); 
+
+
+    }
+
+    public static int UniTotal(string str)
+    {
+   
+        int total = 0;
+
+        foreach (char c in str)
+        {
+            total += ((int)c);
+        }
+        return total;
+    }
+       
+
+    static int TotalSun( string str)
+    {
+        
+        return str.Sum(x => x);
+    }
+    
+    
+    public static int UniTotal2(string str)
+     => str.Sum(x=>(int)x);
+
+}
