61fa6e0c0e84a3ac0848b32edd45b919a4385473
Sophia <83651475+sophialai123@users.noreply.github.com>
2023-02-25 13:32:37 +1100

    Create StrinEndswith.cs

---
+++ C#/Codewars/7ku/StrinEndswith.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Linq;
+using System.Text;
+
+/*
+Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+solution('abc', 'bc') // returns true
+solution('abc', 'd') // returns false
+ */
+
+public static class Kata
+{
+    static void Main(string[] args)
+    {
+        
+
+        var result = Solution("abc", "bc");
+
+        Console.WriteLine(result);
+
+    }
+
+    public static bool Solution(string str, string ending)
+    {
+        // TODO: complete
+
+        return true;
+    }
+
+
+}
